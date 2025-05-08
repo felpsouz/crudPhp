da153a2f3baa6b63613e8051e329be890ed69a76
felpsouz <felipes1807@gmail.com>
2025-05-08 11:13:18 -0300

    update att
    tela inicial e criação de botões para ações futuras

---
+++ index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Usuarios</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+  </head>
+  <body>
+    <?php include('navbar.php'); ?> 
+    <div class="container mt-4">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header">
+              <h4> Lista de Usuarios:
+                <a href="usuarioCreate.php" class="btn btn-primary float-end">Adicionar usuário</a>
+              </h4>
+            </div>
+            <div class="card-body">
+              <table class="table table-bordered table-striped">
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>Data de Nascimento</th>
+                    <th>Ações</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>Teste</td>
+                    <td>teste@gmail.com</td>
+                    <td>01/01/2020</td>
+                    <td>
+                      <a href="" class="btn btn-secondary btn-sm">Visualizar</a>
+                      <a href="" class="btn btn-success btn-sm">Editar</a>
+                      <form action="" method="POST" class="d-inline">
+                      <button type="submit" name="deleteUsuario" value="1" class="btn btn-danger btn-button btn-sm">Excluir</button>
+                      </form>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+  </body>
+</html>
