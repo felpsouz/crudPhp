c3bf3536d7ef5893dbf2797ccba5e1301447beeb
felpsouz <felipes1807@gmail.com>
2025-05-08 11:31:27 -0300

    inicio tela de adicionar
    inicio da tela do create, feito a segunda tela com botão que redireciona para a pagina do index.php

---
+++ usuarioCreate.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Usuarios</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+  </head>
+  <body>
+    <?php include('navbar.php'); ?>
+    <div class="container mt-5">
+        <div class=row>
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Adicionar Usuário
+                            <a href="index.php" class="btn btn-danger float-end">Voltar</a>
+                        </h4>            
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+  </body>
+</html>
